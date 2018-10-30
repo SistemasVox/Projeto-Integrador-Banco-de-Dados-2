7426a3ccc081ed3f311cfdfaf1238e30d7b9752c
Marcelo Vieira <dj.marcelo.2009@gmail.com>
2018-10-30 20:46:07 -0300

    Select do Prodimento OK fix BUG (soma total), implentar Procedimento (50%) ainda.

--- Testes Hotel.sql
+++ Testes Hotel.sql
@@ -8,4 +8,5 @@ select * from pagamento;
 
 SELECT pagamento FROM Pagamento WHERE ID = 0;
 
-select format(1000000000, 2);
+select format(1000000000, 2, 'de_DE'); -- No format, fazemos usado do locale, passando como valor o Alemão.
+-- http://paposql.blogspot.com/2011/12/funcao-para-formatar-moeda-em-reais-no.html
